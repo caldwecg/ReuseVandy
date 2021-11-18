19db3f732c98a579cefbfa95b7d08c6d758ecbad
Carter Caldwell <69778732+caldwecg@users.noreply.github.com>
2021-11-17 22:02:15 -0600

    new push

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.use(express.static("public"))
 
 //Connects project to database
 mongoose.connect('mongodb+srv://caldwecg:ReuseVandy22!@cluster0.htvhh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
